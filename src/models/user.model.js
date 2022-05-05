eb861b93473e7f0c28958ba4125e5e0201c23c54
Landon Mckell <landon.roney7923@gmail.com>
2022-05-05 11:53:57 -0600

    action: added jsonwebtoken to server, updated user model to hash passwords, created generatetoken function

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,19 +1,53 @@
 import mongoose from "mongoose";
 const { Schema } = mongoose;
+const SALT_WORK_FACTOR = 12;
 
 const userSchema = new Schema({
-  firstName: String,
-  lastName: String,
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
   email: {
     type: String,
     unique: true,
     required: true,
   },
-  password: String,
+  password: {
+    type: String,
+    required: true,
+  },
   phoneNumber: String,
   businessNumber: String,
   businessLogo: String,
 });
 
 const User = mongoose.model("User", userSchema);
+
+userSchema.pre(save, function (next) {
+  var user = this;
+
+  if (!user.isModified("password")) return next();
+
+  bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
+    if (err) return next(err);
+
+    bcrypt.hash(user.password, salt, function (err, hash) {
+      if (err) return next(err);
+
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+userSchema.methods.comparePassword = function (candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
 export default User;
